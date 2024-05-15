1a31b215a5a864b4b7bd8a09f89a7e284bba3b09
Sergey <83376337+freak12techno@users.noreply.github.com>
2024-05-15 16:46:35 +0300

    feat: add validate-config command (#52)

--- cmd/cosmos-validators-exporter.go
+++ cmd/cosmos-validators-exporter.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"main/pkg"
+	configPkg "main/pkg/config"
 	"main/pkg/logger"
 
 	"github.com/spf13/cobra"
@@ -11,20 +12,43 @@ var (
 	version = "unknown"
 )
 
-func Execute(configPath string) {
+func ExecuteMain(configPath string) {
 	app := pkg.NewApp(configPath, version)
 	app.Start()
+}
+
+func ExecuteValidateConfig(configPath string) {
+	config, err := configPkg.GetConfig(configPath)
+	if err != nil {
+		logger.GetDefaultLogger().Fatal().Err(err).Msg("Could not load config!")
+	}
+
+	if err := config.Validate(); err != nil {
+		logger.GetDefaultLogger().Fatal().Err(err).Msg("Config is invalid!")
+	}
+
+	config.DisplayWarnings(logger.GetDefaultLogger())
+	logger.GetDefaultLogger().Info().Msg("Provided config is valid.")
 }
 
 func main() {
 	var ConfigPath string
 
 	rootCmd := &cobra.Command{
-		Use:     "cosmos-validators-exporter",
+		Use:     "cosmos-validators-exporter --config [config path]",
 		Long:    "Scrapes validators info on multiple chains.",
 		Version: version,
 		Run: func(cmd *cobra.Command, args []string) {
-			Execute(ConfigPath)
+			ExecuteMain(ConfigPath)
+		},
+	}
+
+	validateConfigCmd := &cobra.Command{
+		Use:     "validate-config --config [config path]",
+		Long:    "Validate config.",
+		Version: version,
+		Run: func(cmd *cobra.Command, args []string) {
+			ExecuteValidateConfig(ConfigPath)
 		},
 	}
 
@@ -32,6 +56,13 @@ func main() {
 	if err := rootCmd.MarkPersistentFlagRequired("config"); err != nil {
 		logger.GetDefaultLogger().Fatal().Err(err).Msg("Could not set flag as required")
 	}
+
+	validateConfigCmd.PersistentFlags().StringVar(&ConfigPath, "config", "", "Config file path")
+	if err := validateConfigCmd.MarkPersistentFlagRequired("config"); err != nil {
+		logger.GetDefaultLogger().Fatal().Err(err).Msg("Could not set flag as required")
+	}
+
+	rootCmd.AddCommand(validateConfigCmd)
 
 	if err := rootCmd.Execute(); err != nil {
 		logger.GetDefaultLogger().Fatal().Err(err).Msg("Could not start application")
