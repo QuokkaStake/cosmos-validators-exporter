17e58b6541506c6e26638e96328f9df820e13b8e
Sergey <freak12techno@gmail.com>
2024-06-15 21:27:06 +0300

    chore: refactor node_signing_info fetcher

--- pkg/fetchers/signing_info.go
+++ pkg/fetchers/signing_info.go
@@ -18,6 +18,12 @@ type SigningInfoFetcher struct {
 	Config *config.Config
 	RPCs   map[string]*tendermint.RPCWithConsumers
 	Tracer trace.Tracer
+
+	wg    sync.WaitGroup
+	mutex sync.Mutex
+
+	queryInfos      []*types.QueryInfo
+	allSigningInfos map[string]map[string]*types.SigningInfoResponse
 }
 
 type SigningInfoData struct {
@@ -41,143 +47,159 @@ func NewSigningInfoFetcher(
 func (q *SigningInfoFetcher) Fetch(
 	ctx context.Context,
 ) (interface{}, []*types.QueryInfo) {
-	var queryInfos []*types.QueryInfo
-
-	allSigningInfos := map[string]map[string]*types.SigningInfoResponse{}
+	q.queryInfos = []*types.QueryInfo{}
+	q.allSigningInfos = map[string]map[string]*types.SigningInfoResponse{}
 
 	for _, chain := range q.Config.Chains {
-		allSigningInfos[chain.Name] = map[string]*types.SigningInfoResponse{}
+		q.allSigningInfos[chain.Name] = map[string]*types.SigningInfoResponse{}
 		for _, consumerChain := range chain.ConsumerChains {
-			allSigningInfos[consumerChain.Name] = map[string]*types.SigningInfoResponse{}
+			q.allSigningInfos[consumerChain.Name] = map[string]*types.SigningInfoResponse{}
 		}
-	}
-
-	var wg sync.WaitGroup
-	var mutex sync.Mutex
-
-	fetchAndSetConsumerKey := func(
-		valoper string,
-		valcons string,
-		chainName string,
-		rpc *tendermint.RPC,
-		mutex *sync.Mutex,
-	) {
-		if valcons == "" {
-			return
-		}
-
-		signingInfo, signingInfoQuery, err := rpc.GetSigningInfo(valcons, ctx)
-
-		mutex.Lock()
-		defer mutex.Unlock()
-
-		queryInfos = append(queryInfos, signingInfoQuery)
-
-		if err != nil {
-			q.Logger.Error().
-				Err(err).
-				Str("chain", chainName).
-				Str("address", valoper).
-				Msg("Error getting validator signing info")
-			return
-		}
-
-		allSigningInfos[chainName][valoper] = signingInfo
 	}
 
 	for _, chain := range q.Config.Chains {
 		rpc, _ := q.RPCs[chain.Name]
 
 		for _, validator := range chain.Validators {
-			wg.Add(1 + len(rpc.Consumers))
+			q.wg.Add(1 + len(rpc.Consumers))
 
-			go func(validator config.Validator, rpc *tendermint.RPC, chain *config.Chain) {
-				defer wg.Done()
-
-				fetchAndSetConsumerKey(
-					validator.Address,
-					validator.ConsensusAddress,
-					chain.Name,
-					rpc,
-					&mutex,
-				)
-			}(validator, rpc.RPC, chain)
+			go q.processProviderChain(
+				ctx,
+				validator,
+				chain.Name,
+				rpc.RPC,
+			)
 
 			for consumerIndex, consumerChain := range chain.ConsumerChains {
 				consumerRPC := rpc.Consumers[consumerIndex]
 
-				go func(validator config.Validator, rpc *tendermint.RPC, providerRPC *tendermint.RPC, chain *config.ConsumerChain) {
-					defer wg.Done()
-
-					if chain.BechConsensusPrefix == "" {
-						return
-					}
-
-					// 1. Fetching assigned key.
-					assignedKey, queryInfo, err := providerRPC.GetConsumerAssignedKey(
-						validator.ConsensusAddress,
-						chain.ChainID,
-						ctx,
-					)
-
-					mutex.Lock()
-					queryInfos = append(queryInfos, queryInfo)
-					mutex.Unlock()
-
-					if err != nil {
-						q.Logger.Error().
-							Err(err).
-							Str("chain", chain.Name).
-							Str("address", validator.Address).
-							Msg("Error getting validator assigned key")
-						return
-					}
-
-					valconsProvider := validator.ConsensusAddress
-					if assignedKey != nil && assignedKey.ConsumerAddress != "" {
-						valconsProvider = assignedKey.ConsumerAddress
-					}
-
-					// 2. Converting it to bech32 prefix of the consumer chain.
-					valcons, err := utils.ChangeBech32Prefix(valconsProvider, chain.BechConsensusPrefix)
-					if err != nil {
-						q.Logger.Error().
-							Err(err).
-							Str("chain", chain.Name).
-							Str("address", validator.Address).
-							Msg("Error converting valcons prefix")
-						return
-					}
-
-					// 3. Converting valoper address on a provider chain to the one on a consumer chain.
-					valoper, err := utils.ChangeBech32Prefix(validator.Address, chain.BechValidatorPrefix)
-					if err != nil {
-						q.Logger.Error().
-							Err(err).
-							Str("chain", chain.Name).
-							Str("address", validator.Address).
-							Msg("Error converting valoper prefix")
-						return
-					}
-
-					// 4. Querying the signing-info on consumer chain.
-					fetchAndSetConsumerKey(
-						valoper,
-						valcons,
-						chain.Name,
-						rpc,
-						&mutex,
-					)
-				}(validator, consumerRPC, rpc.RPC, consumerChain)
+				go q.processConsumerChain(ctx, validator, consumerRPC, rpc.RPC, consumerChain)
 			}
 		}
 	}
 
-	wg.Wait()
+	q.wg.Wait()
 
-	return SigningInfoData{SigningInfos: allSigningInfos}, queryInfos
+	return SigningInfoData{SigningInfos: q.allSigningInfos}, q.queryInfos
 }
 
 func (q *SigningInfoFetcher) Name() constants.FetcherName {
 	return constants.FetcherNameSigningInfo
+}
+
+func (q *SigningInfoFetcher) fetchAndSetSigningInfo(
+	ctx context.Context,
+	valoper string,
+	valcons string,
+	chainName string,
+	rpc *tendermint.RPC,
+) {
+	if valcons == "" {
+		return
+	}
+
+	signingInfo, signingInfoQuery, err := rpc.GetSigningInfo(valcons, ctx)
+
+	q.mutex.Lock()
+	defer q.mutex.Unlock()
+
+	q.queryInfos = append(q.queryInfos, signingInfoQuery)
+
+	if err != nil {
+		q.Logger.Error().
+			Err(err).
+			Str("chain", chainName).
+			Str("address", valoper).
+			Msg("Error getting validator signing info")
+		return
+	}
+
+	q.allSigningInfos[chainName][valoper] = signingInfo
+}
+
+func (q *SigningInfoFetcher) processProviderChain(
+	ctx context.Context,
+	validator config.Validator,
+	chainName string,
+	rpc *tendermint.RPC,
+) {
+	defer q.wg.Done()
+
+	q.fetchAndSetSigningInfo(
+		ctx,
+		validator.Address,
+		validator.ConsensusAddress,
+		chainName,
+		rpc,
+	)
+}
+
+func (q *SigningInfoFetcher) processConsumerChain(
+	ctx context.Context,
+	validator config.Validator,
+	rpc *tendermint.RPC,
+	providerRPC *tendermint.RPC,
+	chain *config.ConsumerChain,
+) {
+	defer q.wg.Done()
+
+	if chain.BechConsensusPrefix == "" {
+		return
+	}
+
+	// 1. Fetching assigned key.
+	assignedKey, queryInfo, err := providerRPC.GetConsumerAssignedKey(
+		validator.ConsensusAddress,
+		chain.ChainID,
+		ctx,
+	)
+
+	q.mutex.Lock()
+	q.queryInfos = append(q.queryInfos, queryInfo)
+	q.mutex.Unlock()
+
+	if err != nil {
+		q.Logger.Error().
+			Err(err).
+			Str("chain", chain.Name).
+			Str("address", validator.Address).
+			Msg("Error getting validator assigned key")
+		return
+	}
+
+	valconsProvider := validator.ConsensusAddress
+	if assignedKey != nil && assignedKey.ConsumerAddress != "" {
+		valconsProvider = assignedKey.ConsumerAddress
+	}
+
+	// 2. Converting it to bech32 prefix of the consumer chain.
+	valcons, err := utils.ChangeBech32Prefix(valconsProvider, chain.BechConsensusPrefix)
+	if err != nil {
+		q.Logger.Error().
+			Err(err).
+			Str("chain", chain.Name).
+			Str("address", validator.Address).
+			Msg("Error converting valcons prefix")
+		return
+	}
+
+	// 3. Converting valoper address on a provider chain to the one on a consumer chain.
+	valoper, err := utils.ChangeBech32Prefix(validator.Address, chain.BechValidatorPrefix)
+	if err != nil {
+		q.Logger.Error().
+			Err(err).
+			Str("chain", chain.Name).
+			Str("address", validator.Address).
+			Msg("Error converting valoper prefix")
+		return
+	}
+
+	// 4. Querying the signing-info on consumer chain.
+	q.fetchAndSetSigningInfo(
+		ctx,
+		valoper,
+		valcons,
+		chain.Name,
+		rpc,
+	)
 }
