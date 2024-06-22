f17910ed645e92f356d437b3308f64cc7a81a249
Sergey <freak12techno@gmail.com>
2024-06-22 19:40:30 +0300

    chore: add denom coefficient generator test

---
+++ pkg/generators/denom_coefficient_test.go
@@ -0,0 +1,35 @@
+package generators
+
+import (
+	"main/pkg/config"
+	statePkg "main/pkg/state"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDenomCoefficientGenerator(t *testing.T) {
+	t.Parallel()
+
+	state := statePkg.NewState()
+	chains := []*config.Chain{
+		{
+			Name: "chain",
+			Denoms: config.DenomInfos{
+				{Denom: "uatom", DisplayDenom: "atom", DenomCoefficient: 1000000},
+			},
+			ConsumerChains: []*config.ConsumerChain{
+				{
+					Name: "consumer",
+					Denoms: config.DenomInfos{
+						{Denom: "uakt", DisplayDenom: "akt", DenomCoefficient: 1000000},
+					},
+				},
+			},
+		},
+	}
+
+	generator := NewDenomCoefficientGenerator(chains)
+	results := generator.Generate(state)
+	assert.NotEmpty(t, results)
+}
