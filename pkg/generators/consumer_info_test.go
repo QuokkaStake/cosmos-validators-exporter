8d886f5658036f301f6207e2ef530d61232c5b7d
Sergey <freak12techno@gmail.com>
2024-06-22 18:53:28 +0300

    chore: add consumer info generator test

---
+++ pkg/generators/consumer_info_test.go
@@ -0,0 +1,39 @@
+package generators
+
+import (
+	"main/pkg/constants"
+	"main/pkg/fetchers"
+	statePkg "main/pkg/state"
+	"main/pkg/types"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConsumerInfoGeneratorNoState(t *testing.T) {
+	t.Parallel()
+
+	state := statePkg.NewState()
+	generator := NewConsumerInfoGenerator()
+	results := generator.Generate(state)
+	assert.Empty(t, results)
+}
+
+func TestConsumerInfoGeneratorNotEmptyState(t *testing.T) {
+	t.Parallel()
+
+	state := statePkg.NewState()
+	state.Set(constants.FetcherNameConsumerInfo, fetchers.ConsumerInfoData{
+		Info: map[string]*types.ConsumerInfoResponse{
+			"chain": {
+				Chains: []types.ConsumerChainInfo{
+					{ChainID: "chain-id", TopN: 1, MinPowerInTopN: "123"},
+				},
+			},
+		},
+	})
+
+	generator := NewConsumerInfoGenerator()
+	results := generator.Generate(state)
+	assert.NotEmpty(t, results)
+}
