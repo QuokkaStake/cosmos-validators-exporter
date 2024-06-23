538a4b99faf8c8fe90f01691971e2f5300ac1711
Sergey <freak12techno@gmail.com>
2024-06-23 13:54:56 +0300

    chore: add slashing params fetcher test

---
+++ pkg/fetchers/slashing_params_test.go
@@ -0,0 +1,255 @@
+package fetchers
+
+import (
+	"context"
+	"errors"
+	"main/assets"
+	"main/pkg/config"
+	"main/pkg/constants"
+	"main/pkg/logger"
+	"main/pkg/tendermint"
+	"main/pkg/tracing"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/jarcoal/httpmock"
+)
+
+func TestSlashingParamsFetcherBase(t *testing.T) {
+	t.Parallel()
+
+	chains := []*config.Chain{
+		{Name: "chain1", LCDEndpoint: "example1"},
+		{Name: "chain2", LCDEndpoint: "example2"},
+	}
+	rpcs := map[string]*tendermint.RPCWithConsumers{
+		"chain1": tendermint.RPCWithConsumersFromChain(
+			chains[0],
+			10,
+			*logger.GetNopLogger(),
+			tracing.InitNoopTracer(),
+		),
+		"chain2": tendermint.RPCWithConsumersFromChain(
+			chains[1],
+			10,
+			*logger.GetNopLogger(),
+			tracing.InitNoopTracer(),
+		),
+	}
+	fetcher := NewSlashingParamsFetcher(
+		logger.GetNopLogger(),
+		chains,
+		rpcs,
+		tracing.InitNoopTracer(),
+	)
+
+	assert.NotNil(t, fetcher)
+	assert.Equal(t, constants.FetcherNameSlashingParams, fetcher.Name())
+}
+
+func TestSlashingParamsFetcherQueryDisabled(t *testing.T) {
+	t.Parallel()
+
+	chains := []*config.Chain{{
+		Name:             "chain",
+		LCDEndpoint:      "example",
+		BechWalletPrefix: "test",
+		Validators:       []config.Validator{{Address: "cosmosvaloper1xqz9pemz5e5zycaa89kys5aw6m8rhgsvw4328e"}},
+		Queries:          map[string]bool{"slashing-params": false},
+	}}
+	rpcs := map[string]*tendermint.RPCWithConsumers{
+		"chain": tendermint.RPCWithConsumersFromChain(
+			chains[0],
+			10,
+			*logger.GetNopLogger(),
+			tracing.InitNoopTracer(),
+		),
+	}
+	fetcher := &SlashingParamsFetcher{
+		Logger: *logger.GetNopLogger(),
+		Chains: chains,
+		RPCs:   rpcs,
+		Tracer: tracing.InitNoopTracer(),
+	}
+	data, queries := fetcher.Fetch(context.Background())
+	assert.Empty(t, queries)
+
+	paramsData, ok := data.(SlashingParamsData)
+	assert.True(t, ok)
+	assert.Empty(t, paramsData.Params)
+}
+
+//nolint:paralleltest // disabled due to httpmock usage
+func TestSlashingParamsFetcherQueryError(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"GET",
+		"https://api.cosmos.quokkastake.io/cosmos/slashing/v1beta1/params",
+		httpmock.NewErrorResponder(errors.New("error")),
+	)
+
+	chains := []*config.Chain{{
+		Name:             "chain",
+		LCDEndpoint:      "https://api.cosmos.quokkastake.io",
+		BechWalletPrefix: "cosmos",
+		Validators:       []config.Validator{{Address: "cosmosvaloper1xqz9pemz5e5zycaa89kys5aw6m8rhgsvw4328e"}},
+	}}
+	rpcs := map[string]*tendermint.RPCWithConsumers{
+		"chain": tendermint.RPCWithConsumersFromChain(
+			chains[0],
+			10,
+			*logger.GetNopLogger(),
+			tracing.InitNoopTracer(),
+		),
+	}
+	fetcher := &SlashingParamsFetcher{
+		Logger: *logger.GetNopLogger(),
+		Chains: chains,
+		RPCs:   rpcs,
+		Tracer: tracing.InitNoopTracer(),
+	}
+	data, queries := fetcher.Fetch(context.Background())
+	assert.Len(t, queries, 1)
+	assert.False(t, queries[0].Success)
+
+	paramsData, ok := data.(SlashingParamsData)
+	assert.True(t, ok)
+	assert.Empty(t, paramsData.Params)
+}
+
+//nolint:paralleltest // disabled due to httpmock usage
+func TestSlashingParamsFetcherNodeError(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"GET",
+		"https://api.cosmos.quokkastake.io/cosmos/slashing/v1beta1/params",
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("error.json")),
+	)
+
+	chains := []*config.Chain{{
+		Name:             "chain",
+		LCDEndpoint:      "https://api.cosmos.quokkastake.io",
+		BechWalletPrefix: "cosmos",
+		Validators:       []config.Validator{{Address: "cosmosvaloper1xqz9pemz5e5zycaa89kys5aw6m8rhgsvw4328e"}},
+	}}
+	rpcs := map[string]*tendermint.RPCWithConsumers{
+		"chain": tendermint.RPCWithConsumersFromChain(
+			chains[0],
+			10,
+			*logger.GetNopLogger(),
+			tracing.InitNoopTracer(),
+		),
+	}
+	fetcher := &SlashingParamsFetcher{
+		Logger: *logger.GetNopLogger(),
+		Chains: chains,
+		RPCs:   rpcs,
+		Tracer: tracing.InitNoopTracer(),
+	}
+	data, queries := fetcher.Fetch(context.Background())
+	assert.Len(t, queries, 1)
+	assert.False(t, queries[0].Success)
+
+	paramsData, ok := data.(SlashingParamsData)
+	assert.True(t, ok)
+	assert.Empty(t, paramsData.Params)
+}
+
+//nolint:paralleltest // disabled due to httpmock usage
+func TestSlashingParamsFetcherQuerySuccess(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"GET",
+		"https://api.cosmos.quokkastake.io/cosmos/slashing/v1beta1/params",
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("slashing-params.json")),
+	)
+
+	chains := []*config.Chain{{
+		Name:             "chain",
+		LCDEndpoint:      "https://api.cosmos.quokkastake.io",
+		BechWalletPrefix: "cosmos",
+		Validators:       []config.Validator{{Address: "cosmosvaloper1xqz9pemz5e5zycaa89kys5aw6m8rhgsvw4328e"}},
+	}}
+	rpcs := map[string]*tendermint.RPCWithConsumers{
+		"chain": tendermint.RPCWithConsumersFromChain(
+			chains[0],
+			10,
+			*logger.GetNopLogger(),
+			tracing.InitNoopTracer(),
+		),
+	}
+	fetcher := &SlashingParamsFetcher{
+		Logger: *logger.GetNopLogger(),
+		Chains: chains,
+		RPCs:   rpcs,
+		Tracer: tracing.InitNoopTracer(),
+	}
+	data, queries := fetcher.Fetch(context.Background())
+	assert.Len(t, queries, 1)
+	assert.True(t, queries[0].Success)
+
+	paramsData, ok := data.(SlashingParamsData)
+	assert.True(t, ok)
+
+	chainData, ok := paramsData.Params["chain"]
+	assert.True(t, ok)
+	assert.Equal(t, int64(10000), chainData.SlashingParams.SignedBlocksWindow.Int64())
+}
+
+//nolint:paralleltest // disabled due to httpmock usage
+func TestSlashingParamsFetcherConsumer(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"GET",
+		"https://api.neutron.quokkastake.io/cosmos/slashing/v1beta1/params",
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("slashing-params.json")),
+	)
+
+	chains := []*config.Chain{{
+		Name:             "chain",
+		LCDEndpoint:      "https://api.cosmos.quokkastake.io",
+		BechWalletPrefix: "cosmos",
+		Validators:       []config.Validator{{Address: "cosmosvaloper1xqz9pemz5e5zycaa89kys5aw6m8rhgsvw4328e"}},
+		Queries:          map[string]bool{"slashing-params": false},
+		ConsumerChains: []*config.ConsumerChain{
+			{
+				Name:             "consumer",
+				LCDEndpoint:      "https://api.neutron.quokkastake.io",
+				BechWalletPrefix: "neutron",
+			},
+		},
+	}}
+	rpcs := map[string]*tendermint.RPCWithConsumers{
+		"chain": tendermint.RPCWithConsumersFromChain(
+			chains[0],
+			10,
+			*logger.GetNopLogger(),
+			tracing.InitNoopTracer(),
+		),
+	}
+	fetcher := &SlashingParamsFetcher{
+		Logger: *logger.GetNopLogger(),
+		Chains: chains,
+		RPCs:   rpcs,
+		Tracer: tracing.InitNoopTracer(),
+	}
+	data, queries := fetcher.Fetch(context.Background())
+	assert.Len(t, queries, 1)
+	assert.True(t, queries[0].Success)
+
+	paramsData, ok := data.(SlashingParamsData)
+	assert.True(t, ok)
+
+	chainData, ok := paramsData.Params["consumer"]
+	assert.True(t, ok)
+	assert.Equal(t, int64(10000), chainData.SlashingParams.SignedBlocksWindow.Int64())
+}
